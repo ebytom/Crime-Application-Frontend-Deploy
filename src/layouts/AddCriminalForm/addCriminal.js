ea046ae7c809c90030ac1d4c5e94fe9205f06054
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-07-29 15:39:51 +0530

    v3

--- src/layouts/AddCriminalForm/addCriminal.js
+++ src/layouts/AddCriminalForm/addCriminal.js
@@ -68,7 +68,7 @@ function AddCriminal() {
             .then((res) => {
                 console.log(res);
                 setLoader(false)
-                // Success()
+                window.location.reload();
             })
             .catch(err => {
                 console.log(err);
