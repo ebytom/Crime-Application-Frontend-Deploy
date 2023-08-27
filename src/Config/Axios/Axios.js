cb82369d6f937559a98026dc827f08fa3b26e0e7
Eby-Tom <ebytomy7@gmail.com>
2023-08-27 14:09:10 +0530

    backend hosted

--- src/Config/Axios/Axios.js
+++ src/Config/Axios/Axios.js
@@ -6,5 +6,6 @@ axios.defaults.headers.get["Content-Type"] = "application/json";
 // axios.defaults.headers.get["Content-Type"] = "multipart/form-data";
 
 export const Axios = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://crime-application-backend.vercel.app/",
 });
